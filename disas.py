d60db1cf84ad3195dbbd5356a6392c756f7c3a8f
lgembarz <lucskig2@gmail.com>
2020-12-08 16:43:30 -0500

    added unintended instrs

---
+++ disas.py
@@ -0,0 +1,35 @@
+# disas.py
+
+from capstone import *
+from elftools.elf.elffile import ELFFile
+from elftools.elf.relocation import RelocationSection
+filename= './vuln_prog2.bin'
+disas = [] #list of tuples: tup[0] = address, tup[1] = instruction, tup[2] = args
+with open(filename, 'rb') as file:
+        elf = ELFFile(file)
+        code = elf.get_section_by_name('.text')
+        ops = code.data() # type bytes
+        # ops2 = ops[-15:]
+        addr = code['sh_addr'] # type int
+        md = Cs(CS_ARCH_X86, CS_MODE_64)
+
+        # gets all instructions, intended or not
+
+        while(ops != b""):
+            for i in md.disasm(ops, addr):
+                disas.append((i.address,i.mnemonic,i.op_str))
+            addr += 1
+            ops = ops[1:]
+
+
+'''
+gadgets = []
+gadgetIndex = 0
+while gadgetIndex < len(disas):
+        if (disas[gadgetIndex][1] == 'pop') and (disas[gadgetIndex+1][1] == 'ret'):
+                gadgets.append((disas[gadgetIndex],disas[gadgetIndex+1]))
+        gadgetIndex = gadgetIndex + 1
+for i in gadgets:
+        print(i)
+        #print('\n')
+'''
