791519aa68757c3fa75634bb50495e28f22b0c22
Hunter Devlin <hdevlin@stevens.edu>
2020-12-08 00:11:11 -0500

    updated print of disas, beginnings of unintended gadgets

--- disas.py
+++ disas.py
@@ -8,12 +8,15 @@ disas = [] #list of tuples: tup[0] = address, tup[1] = instruction, tup[2] = arg
 with open(filename, 'rb') as file:
         elf = ELFFile(file)
         code = elf.get_section_by_name('.text')
+        print(type(code))
+        print(code)
         ops = code.data()
+        ops2 = ops[-17:] #gives unintended gadget 17 bytes from end, addresses are wrong
         addr = code['sh_addr']
         md = Cs(CS_ARCH_X86, CS_MODE_64)
         for i in md.disasm(ops,addr):
                 print(f'0x{i.address:x}\t{i.mnemonic}\t{i.op_str}')
-                disas.append((i.address,i.mnemonic,i.op_str))
+                disas.append((hex(i.address),i.mnemonic,i.op_str))
         for section in elf.iter_sections():
                 if isinstance(section, RelocationSection):
                         print(f'{section.name}:')
@@ -28,5 +31,7 @@ while gadgetIndex < len(disas):
         if (disas[gadgetIndex][1] == 'pop') and (disas[gadgetIndex+1][1] == 'ret'):
                 gadgets.append((disas[gadgetIndex],disas[gadgetIndex+1]))
         gadgetIndex = gadgetIndex + 1
-print(gadgets)
+for i in gadgets:
+        print(i)
+        #print('\n')
 
