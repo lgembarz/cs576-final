5918cf0325dc51ca09a010074ec160712cd5723e
Hunter Devlin <hdevlin@stevens.edu>
2020-12-07 23:09:41 -0500

    added vuln_prog2 and modifications to disas

--- disas.py
+++ disas.py
@@ -3,7 +3,8 @@
 from capstone import *
 from elftools.elf.elffile import ELFFile
 from elftools.elf.relocation import RelocationSection
-filename= './vuln_prog1.bin'
+filename= './vuln_prog2.bin'
+disas = [] #list of tuples: tup[0] = address, tup[1] = instruction, tup[2] = args
 with open(filename, 'rb') as file:
         elf = ELFFile(file)
         code = elf.get_section_by_name('.text')
@@ -12,6 +13,7 @@ with open(filename, 'rb') as file:
         md = Cs(CS_ARCH_X86, CS_MODE_64)
         for i in md.disasm(ops,addr):
                 print(f'0x{i.address:x}\t{i.mnemonic}\t{i.op_str}')
+                disas.append((i.address,i.mnemonic,i.op_str))
         for section in elf.iter_sections():
                 if isinstance(section, RelocationSection):
                         print(f'{section.name}:')
@@ -20,3 +22,11 @@ with open(filename, 'rb') as file:
                                 symbol = symbol_table.get_symbol(relocation['r_info_sym'])
                                 addr = hex(relocation['r_offset'])
                                 print(f'{symbol.name} {addr}')
+gadgets = []
+gadgetIndex = 0
+while gadgetIndex < len(disas):
+        if (disas[gadgetIndex][1] == 'pop') and (disas[gadgetIndex+1][1] == 'ret'):
+                gadgets.append((disas[gadgetIndex],disas[gadgetIndex+1]))
+        gadgetIndex = gadgetIndex + 1
+print(gadgets)
+
