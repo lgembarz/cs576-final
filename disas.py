7fff812f8752f0195c957317893fb617bac50d51
lgembarz <lucskig2@gmail.com>
2020-12-09 18:53:13 -0500

    before merge, find smallest

--- disas.py
+++ disas.py
@@ -23,6 +23,8 @@ with open(filename, 'rb') as file:
 
 gadgets = []
 gadgetIndex = 0
+
+
 while gadgetIndex < len(disas):
         if (disas[gadgetIndex][1] == 'pop') and (disas[gadgetIndex+1][1] == 'ret'):
                 gadgets.append((disas[gadgetIndex],disas[gadgetIndex+1]))
@@ -30,6 +32,25 @@ while gadgetIndex < len(disas):
                 gadgets.append((disas[gadgetIndex],disas[gadgetIndex+1],disas[gadgetIndex+2]))
 
         gadgetIndex = gadgetIndex + 1
+
+'''
+for reg_tup in register_tuple:
+    for x in range(0, len(gadgets)):
+        if (gadgets[x][2] == reg_tup[1]) and (gadgets[x][1] < reg_tup[2]):
+            reg_tup[2] = gadgets[x][1]
+'''
+
+
+'''
+while gadgetIndex < len(disas):
+    for distance_to_ret in range(1,6):
+        for reg in [("rdi", "rsi", "rdx"]:
+            if (disas[gadgetIndex][1] == 'pop') and (disas[gadgetIndex+distance_to_ret][1] == 'ret'):
+                gadgets.append((disas[gadgetIndex],disas[gadgetIndex+1]))
+
+
+    gadgetIndex = gadgetIndex + 1
+'''
 
 #remove duplicates
 gadgets.sort()
@@ -53,3 +74,22 @@ while gadgetIndex < len(gadgets):
 
 for i in gadgets:
         print(i)
+
+shortest_rdi = 100
+shortest_rsi = 100
+shortest_rdx = 100
+
+for x in range(0, len(gadgets)):
+    if (gadgets[x][0][2] == "rdi") and (len(gadgets[x]) < shortest_rdi):
+        shortest_rdi = len(gadgets[x])
+    elif (gadgets[x][0][2] == "rsi") and (len(gadgets[x]) < shortest_rsi):
+        shortest_rsi = len(gadgets[x])
+    elif (gadgets[x][0][2] == "rdx") and (len(gadgets[x]) < shortest_rdx):
+        shortest_rdx = len(gadgets[x])
+    else:
+        print("uhoh")
+
+print(shortest_rdi)
+print(shortest_rsi)
+print(shortest_rdx)
+
